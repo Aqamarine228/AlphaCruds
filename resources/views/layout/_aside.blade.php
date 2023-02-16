be7f5b1f3c5496e0f760c221a530c00e1849d4f2
Aqamarine228 <MrFurgJK@gmail.com>
2023-02-16 19:39:42 +0100

    task completed

---
+++ resources/views/layout/_aside.blade.php
@@ -0,0 +1,24 @@
+<aside class="main-sidebar sidebar-light-primary elevation-3">
+    <a href="#" class="brand-link">
+        <img src="{{asset('vendor/alphacruds/img/logo.png')}}" alt="Logo" class="brand-image img-circle elevation-3"
+             style="opacity: .8">
+        <span class="brand-text font-weight-light">AlphaCruds</span>
+    </a>
+
+    <div class="sidebar">
+
+        <nav class="mt-2" aria-label="Aside navigation">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+                <li class="nav-item">
+                    <a href="{{route('alphacruds.dashboard')}}" class="nav-link">
+                        <em class="nav-icon fas fa-tachometer-alt"></em>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
