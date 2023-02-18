34a8845ac0ab12ace21d1614514dcb24033b3491
Aqamarine228 <MrFurgJK@gmail.com>
2023-02-18 08:15:12 +0100

    added optional parameters and fixed stub bugs

--- src/Commands/CrudMakeCommand.php
+++ src/Commands/CrudMakeCommand.php
@@ -8,6 +8,7 @@ use Nwidart\Modules\Commands\GeneratorCommand;
 use Nwidart\Modules\Support\Config\GenerateConfigReader;
 use Nwidart\Modules\Traits\ModuleCommandTrait;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class CrudMakeCommand extends GeneratorCommand
 {
@@ -82,6 +83,15 @@ class CrudMakeCommand extends GeneratorCommand
         ];
     }
 
+    protected function getOptions(): array
+    {
+        return [
+            ['model-namespace', 'mn', InputOption::VALUE_OPTIONAL, 'Namespace of model which will CRUD class use.'],
+            ['base', 'b', InputOption::VALUE_OPTIONAL, 'Namespace of class which will CRUD class extend.'],
+            ['request', 'r', InputOption::VALUE_OPTIONAL, 'Namespace of request which will CRUD class use for validation.'],
+        ];
+    }
+
     private function getModelCamelName(): string
     {
         return Str::camel($this->getModelName());
@@ -94,8 +104,11 @@ class CrudMakeCommand extends GeneratorCommand
 
     public function getModelNamespace(): string
     {
-        $path = $this->laravel['modules']->config('paths.generator.model.path', 'Models');
+        if ($this->option('model-namespace')) {
+            return $this->option('model-namespace');
+        }
 
+        $path = $this->laravel['modules']->config('paths.generator.model.path', 'Models');
         $path = str_replace('/', '\\', $path);
 
         return $this->laravel['modules']->config('namespace')
@@ -107,6 +120,10 @@ class CrudMakeCommand extends GeneratorCommand
 
     public function getRequestPath(): string
     {
+        if ($this->option('request')) {
+            return $this->option('request');
+        }
+
         $path = $this->laravel['modules']->config('paths.generator.request.path', 'Http/Requests');
         $path = str_replace('/', '\\', $path);
 
@@ -123,6 +140,10 @@ class CrudMakeCommand extends GeneratorCommand
 
     public function getBaseControllerPath(string $module): string
     {
+        if ($this->option('base')) {
+            return $this->option('base');
+        }
+
         $path = $this->laravel['modules']->config('paths.generator.controller.path', 'Http/Controllers');
         $path = str_replace('/', '\\', $path);
 
