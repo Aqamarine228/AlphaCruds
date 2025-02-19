2f4144ad1ae3d6b513e0e92acd04c8d915bf616b
Aqamarine228 <MrFurgJK@gmail.com>
2025-02-19 10:25:32 +0100

    fixed route tests

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -6,8 +6,8 @@ class RoutesTest extends AlphaCrudsTestCase
 {
     public function testItOpensRoutes(): void
     {
-        $this->get(route('alphacruds.crud-generator'))->assertStatus(200);
-        $this->get(route('alphacruds.translated-crud-generator'))->assertStatus(200);
-        $this->get(route('alphacruds.api-crud-generator'))->assertStatus(200);
+        $this->get(route('alphacruds.crud-generator'))->assertOk();
+        $this->get(route('alphacruds.translated-crud-generator'))->assertOk();
+        $this->get(route('alphacruds.api-crud-generator'))->assertOk();
     }
 }
